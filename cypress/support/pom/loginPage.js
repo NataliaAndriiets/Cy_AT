bdf8cf0e799242c52b6ee24e8a9f3e5340dd3675
Natalia Andriiets <nandriiets@mandmdirect.com>
2025-03-27 21:20:58 +0000

    P

---
+++ cypress/support/pom/loginPage.js
@@ -0,0 +1,30 @@
+﻿
+class LoginPage {
+ 
+  get emailInput() {
+    return cy.get('#signinEmail');
+  }
+
+  get passwordInput() {
+    return cy.get('#password');
+  }
+
+  get loginButton() {
+    return cy.get('button[type="submit"]');
+  }
+
+  typeUsername(username) {
+    this.usernameInput.type(username);
+    return this;
+  }
+
+  typePassword(password) {
+    this.passwordInput.type(password);
+    return this;
+  }
+
+  clickLoginButton() {
+    this.loginButton.click();
+  }
+}
+export default new LoginPage();
